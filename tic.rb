86413018618c3b180426f62f1c5837a4d8dde421
Rene Kumsta <rck@sequensis.de>
2023-10-08 14:19:25 +0700

    First commit
    Board print out

---
+++ tic.rb
@@ -0,0 +1,25 @@
+class Player
+  def initialize(name)
+    @name = name
+  end
+
+
+end
+
+class Board
+  def initialize
+    puts '1 | 2 | 3
+_ _ _ _ _
+
+4 | 5 | 6
+_ _ _ _ _
+
+7 | 8 | 9'
+  end
+
+  def update(field_pos)
+
+  end
+end
+
+p board = Board.new
